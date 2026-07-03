6614857e987bbb5906b70c7fb7c4e415eb660ad3
Vinil <134795493+vinil76@users.noreply.github.com>
2026-07-03 20:59:35 +0400

    Time: 1 ms (99.55%), Space: 55.5 MB (40.76%) - LeetHub

---
+++ 1013-partition-array-into-three-parts-with-equal-sum/1013-partition-array-into-three-parts-with-equal-sum.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean canThreePartsEqualSum(int[] arr) {
+        int total_sum = 0;
+
+        for(int val:arr){
+            total_sum += val;
+        }
+
+        if(total_sum % 3 != 0)return false;
+
+        int count = 0;
+        int cur_sum = 0;
+        int target = total_sum / 3;
+
+
+        for(int val:arr){
+            cur_sum += val;
+            if(cur_sum == target){
+                count += 1;
+                cur_sum = 0;
+            }
+        }
+        
+
+        return count >= 3;       
+    }
+}
